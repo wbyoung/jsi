04460b3721b7c0305d672dcaadc48ec5f969d0a2
Whitney Young <wbyoung@fadingred.com>
2014-03-27 09:54:10 -0700

    Added anchor links.

---
+++ scripts/local.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('h1, h2, h3, h4, h5, h6').each(function() {
+    $this = $(this);
+    if (!$this.attr('id')) {
+      var name = $this.html().toLowerCase().replace(/[^\w]+/gi, '_');
+      var icon = $('<i/>').addClass('fa fa-link');
+      var anchor = $('<a/>').addClass('section-reference').attr('href', '#' + name);
+      anchor.append(icon);
+      $this.attr('id', name);
+      $this.prepend(anchor);
+    }
+  });
+});
